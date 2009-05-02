ea0b418bfe16b602dc8260e06c18a9d5e2ee7600
Greg Hewgill <greg@hewgill.com>
2009-05-02 12:00:19 +1200

    detection of version required for imported module

--- pyqver.py
+++ pyqver.py
@@ -1,13 +1,32 @@
+import collections
 import compiler
+
+StandardModules = {
+    "hashlib": (2, 5),
+}
+
+class NodeChecker(object):
+    def __init__(self):
+        self.vers = collections.defaultdict(list)
+        self.vers[(2,0)].append(None)
+    def visitImport(self, node):
+        for n in node.names:
+            v = StandardModules.get(n[0])
+            if v is not None:
+                self.vers[v].append(n)
 
 def qver(source):
     """Return the minimum Python version required to run a particular bit of code.
 
     >>> qver('print "hello world"')
     (2, 0)
+    >>> qver('import hashlib')
+    (2, 5)
     """
     tree = compiler.parse(source)
-    return (2, 0)
+    checker = NodeChecker()
+    compiler.walk(tree, checker)
+    return max(checker.vers.keys())
 
 if __name__ == "__main__":
     import doctest
